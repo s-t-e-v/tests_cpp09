0d6af05c51d17fa28f9748f0824e6d65fe71e9af
s-t-e-v <steven.bandaogo@proton.me>
2024-12-30 18:26:02 +0100

    refact test file + finish .sh tester

--- ex01/tests.sh
+++ ex01/tests.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-prog_name=$1
-
 # ANSI codes for colors
 GREEN="\033[32m"
 RED="\033[31m"
@@ -14,52 +12,46 @@ END="\033[m"
 TESTS_DIR="../tests/ex01"
 
 # Binary of your program
-BINARY=$prog_name
+BINARY="./$1"
 
 # Counters
 nb_passed=0
 nb_tests=0
 
+tests_file="$TESTS_DIR/tests"
 
-# Outer loop to iterate over test types (directories in $TESTS_DIR)
-for test_type_dir in "$TESTS_DIR"/*; do
+while IFS= read -r line; do
+    if [[ "$line" =~ ^# ]]; then
+        test_name="${line:2}"
 
-    # Check if it is a directory
-    if [ -d "$test_type_dir" ]; then
-        test_name=$(basename "$test_type_dir")
-        echo -e "${BOLD}---- $test_name input tests ----\n${END}"
+        read -r arg
+        read -r expected_output
 
-        # Inner loop: iterate over test files in each test_name directory
-        for test_file in "$test_type_dir"/*; do
-            echo -n "Testing: $test_file"
-            ((nb_tests++))
+        echo -ne "Test: $test_name: ${YELLOW}$arg${END} -> "
+        echo -ne "${CYAN}$expected_output${END}"
 
-            test_passed=true
-            # Get argument
-            arg=$(head -n 1 $test_file)
+        ((nb_tests++))
 
-            expected_output=expected_output
-            actual_output=actual_output
-            # Run the program with the argument in the first line of test_file
-            $BINARY "$arg" 2>/dev/null > $actual_output
-            actual_type=$?
+        # Run the RPN program and capture its output
+        actual_output=$($BINARY "$arg" 2> /dev/null)
 
-            tail -n 4 $test_file > $expected_output
+        # Boolean checks
+        is_expected_error=$([[ "$expected_output" == "Error" && \
+                               "$actual_output" == Error* ]])
+        is_exact_match=$([[ "$expected_output" != "Error" && \
+                            "$actual_output" == "$expected_output" ]])
 
-            # Check the actual_type and print the corresponding message
-            if [ $actual_type -eq "${expected_type[$test_name]}" ] ; then
-                echo -ne "${GREEN} OK${END}"
-            else
-                echo -ne "${RED} KO${END}"
-                test_passed=false
-            fi
-
-            echo ""
-        done
+        # Evaluate result
+        if $is_expected_error || $is_exact_match; then
+            echo -ne "${GREEN} OK${END}"
+            ((nb_passed++))
+        else
+            echo -ne "${RED} KO${END}"
+        fi
 
         echo ""
     fi
-done
+done < "$tests_file"
 
 echo ""
 
@@ -69,4 +61,3 @@ if [ $nb_passed -eq $nb_tests ]; then
 else
     echo -e "${RED}${nb_passed}/${nb_tests} tests passed${END}"
 fi
-
